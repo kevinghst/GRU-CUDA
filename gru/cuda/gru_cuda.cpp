284588447f93660e38dc4db0664761bfee8bca37
Wancong Zhang <wancongzhang@gmail.com>
2019-10-30 21:42:43 -0400

    add back contiguous

--- gru/cuda/gru_cuda.cpp
+++ gru/cuda/gru_cuda.cpp
@@ -28,7 +28,7 @@ std::vector<torch::Tensor> gru_cuda_backward(
 // NOTE: AT_ASSERT has become AT_CHECK on master after 0.4.
 #define CHECK_CUDA(x) AT_ASSERTM(x.type().is_cuda(), #x " must be a CUDA tensor")
 #define CHECK_CONTIGUOUS(x) AT_ASSERTM(x.is_contiguous(), #x " must be contiguous")
-#define CHECK_INPUT(x) CHECK_CUDA(x);
+#define CHECK_INPUT(x) CHECK_CUDA(x); CHECK_CONTIGUOUS(x)
 
 std::vector<torch::Tensor> gru_forward(
     torch::Tensor input,
